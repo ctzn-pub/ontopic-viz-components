0120739f16b1849c274bd3abbacd262498e7b68b
Uma Huggins <uma@ctzn.pub>
2025-11-19 22:59:16 -0500

    Export UI components (Label, Switch, Button, Input, Tabs) from package
    - TimeTrendDemoChart requires these UI components
    - Previously they existed but weren't exported
    - Fixes chart rendering issues when component can't find dependencies

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,3 +1,8 @@
 // UI components
 export { default as StateDataTable } from './StateDataTable';
 export { default as CorrelationHeatmapSvg } from './CorrelationHeatmapSvg';
+export { Label } from './label';
+export { Switch } from './switch';
+export { Button } from './button';
+export { Input } from './input';
+export { Tabs, TabsContent, TabsList, TabsTrigger } from './tabs';
